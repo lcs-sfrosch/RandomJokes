b72790df9723d6af03661d2a699e5d8fab4d8b46
Sebastian Frosch <sfrosch@lcs.on.ca>
2023-04-14 11:27:29 -0400

    Initial Commit

---
+++ RandomJokes/RandomJokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomJokesApp.swift
+//  RandomJokes
+//
+//  Created by Sebastian Frosch on 14.04.23.
+//
+
+import SwiftUI
+
+@main
+struct RandomJokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
